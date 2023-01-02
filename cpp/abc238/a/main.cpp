56c6ce3eb97104b56436b961670a555dd19b8177
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-03 02:28:38 +0900

    add: abc238a

---
+++ cpp/abc238/a/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    if (pow(2, n) > pow(n, 2)) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
