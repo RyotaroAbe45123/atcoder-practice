39245558db40b1d9034164c6c4acb128455b2793
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-03 17:45:17 +0900

    update: abc234a

--- cpp/abc234/a/main.cpp
+++ cpp/abc234/a/main.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long f(long long x) {
-    return pow(x, 2) + (2 * x) + 3;
+int f(int x) {
+    return x * x + 2 * x + 3;
 }
 
 int main() {
     int t;
     cin >> t;
     long long res;
-    res = f(f(f(t) + t) + f(f(t)));
-    cout << res << endl;
+    cout << f(f(f(t) + t) + f(f(t))) << endl;
 }
