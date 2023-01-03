0c9b3f35340ce61107de2ff47272604846f0b896
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-03 18:35:26 +0900

    add: abc230a

---
+++ cpp/abc230/a/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    string s;
+    if (N < 10) {
+        cout << "AGC00" << to_string(N) << endl;
+    }
+    else if (N < 42) {
+        cout << "AGC0" << to_string(N) << endl;
+    }
+    else {
+        cout << "AGC0" << to_string(N+1) << endl;
+    }
+    return 0;
+}
