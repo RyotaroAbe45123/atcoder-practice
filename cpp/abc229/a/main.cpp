5e3cc74ac120257ff71626d837cd431f9e177939
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-03 19:54:28 +0900

    update: abc229a

--- cpp/abc229/a/main.cpp
+++ cpp/abc229/a/main.cpp
@@ -2,18 +2,16 @@
 using namespace std;
 
 int main() {
-    vector<string> s(2);
-    for (int i = 0; i < 2; i++) cin >> s[i];
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            if (s[i][j] == '#') {
-                if ((s[(i+1)%2][j] != '#') && (s[i][(j+1)%2] != '#')) {
-                    cout << "No" << endl;
-                    return 0;
-                }
-            }
-        }
+    string s1, s2;
+    cin >> s1 >> s2;
+    if (s1 == "#." && s2 == ".#") {
+        cout << "No" << endl;
     }
-    cout << "Yes" << endl;
+    else if (s1 == ".#" && s2 == "#.") {
+        cout << "No" << endl;
+    }
+    else {
+        cout << "Yes" << endl;
+    }
     return 0;
 }
