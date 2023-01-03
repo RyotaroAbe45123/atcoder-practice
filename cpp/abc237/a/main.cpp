b297c16542f9794ecf706035bc7a0f6855e07519
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-03 14:53:14 +0900

    update: abc237a

--- cpp/abc237/a/main.cpp
+++ cpp/abc237/a/main.cpp
@@ -5,7 +5,7 @@ int main() {
     long long n;
     cin >> n;
     long long x = (long long)1 << 31;
-    if ((-x <= n) && (n <= x)) {
+    if ((-x <= n) && (n < x)) {
         cout << "Yes" << endl;
     }
     else {
