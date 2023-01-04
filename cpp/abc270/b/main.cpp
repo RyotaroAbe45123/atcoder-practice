24e3104fe7d3575dd9d6101d9670efd8b4d78f96
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-04 22:00:26 +0900

    upate: abc270b

--- cpp/abc270/b/main.cpp
+++ cpp/abc270/b/main.cpp
@@ -5,16 +5,16 @@ int main() {
     int x, y, z;
     cin >> x >> y >> z;
     if (y < 0) {
-        x -= x;
-        y -= y;
-        z -= z;
+        x = -x;
+        y = -y;
+        z = -z;
     }
     if (x < y) {
         cout << abs(x) << endl;
     }
     else {
         if (z > y) {
-            cout << -1 << endl;
+            cout << "-1" << endl;
         }
         else {
             cout << abs(z) + abs(x - z) << endl;
