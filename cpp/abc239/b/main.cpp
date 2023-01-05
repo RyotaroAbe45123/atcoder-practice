1b0081638814c4ce7950b22217bbc9422aede987
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-05 12:33:40 +0900

    update: abc239b

--- cpp/abc239/b/main.cpp
+++ cpp/abc239/b/main.cpp
@@ -5,7 +5,7 @@ int main() {
     long long x;
     cin >> x;
     if (x < 0) {
-        cout << (x / 10) + 1 << endl;
+        cout << (x / 10) - 1 << endl;
     }
     else {
         cout << x / 10 << endl;
