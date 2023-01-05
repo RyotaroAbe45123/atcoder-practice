830dfc05fc519f939681b147b4272fc7e2f97584
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-05 12:44:11 +0900

    update: abc239b

--- cpp/abc239/b/main.cpp
+++ cpp/abc239/b/main.cpp
@@ -4,10 +4,5 @@ using namespace std;
 int main() {
     long long x;
     cin >> x;
-    if (x < 0 && x % 10 != 0) {
-        cout << (x / 10) - 1 << endl;
-    }
-    else {
-        cout << x / 10 << endl;
-    }
+    cout << (x / 10) - (x % 10 < 0) << endl;
 }
