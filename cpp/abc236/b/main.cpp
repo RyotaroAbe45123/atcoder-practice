1493447235164d1c22301ea2685bd8c38a8c37a4
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-05 21:03:17 +0900

    update: abc237b

--- cpp/abc236/b/main.cpp
+++ cpp/abc236/b/main.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    vector<int> a(n, 0);
+    vector<int> a(n + 1, 0);
     for (int i = 0; i < 4 * n - 1; i++) {
         int ai;
         cin >> ai;
-        a[ai-1]++;
+        a[ai] += 1;
     }
-    for (int i = 0; i < n; i++) {
-        if (a[i] != 4) {
-            cout << i+1 << endl;
+    for (int i = 1; i <= n; i++) {
+        if (a[i] == 3) {
+            cout << i << endl;
             return 0;
         }
     }
