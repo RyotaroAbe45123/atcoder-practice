35293e9bd5dc6f3865f33be259002c562d37b33f
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-05 22:08:22 +0900

    update: abc232b

--- cpp/abc232/b/main.cpp
+++ cpp/abc232/b/main.cpp
@@ -4,11 +4,12 @@ using namespace std;
 int main() {
     string s, t;
     cin >> s >> t;
-    for (int i = 0; i < 26; i++) {
-        for (int j = 0; j < s.size(); j++) {
-            s[j] = char('a' + ((s[j] - 'a') + i) % 26);
+    for (int k = 0; k < 26; k++) {
+        string s2 = s;
+        for (int j = 0; j < (int)s.size(); j++) {
+            s2[j] = ((s2[j] - 'a') + k) % 26 + 'a';
         }
-        if (s == t) {
+        if (s2 == t) {
             cout << "Yes" << endl;
             return 0;
         }
