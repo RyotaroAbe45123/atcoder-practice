6726f139adec4f7fa455c027dc2faca4096a8776
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-05 21:45:30 +0900

    update: abc233b

---
+++ cpp/abc233/b/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int l, r;
+    string s;
+    cin >> l >> r >> s;
+    l--; r--;
+    int p = l, q = r;
+    while (p < q) {
+        swap(s[p], s[q]);
+        p++; q--;
+    }
+    cout << s << endl;
+    return 0;
+}
