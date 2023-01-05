2808e638f18d110d0e99810ab50c957226e96520
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-05 20:52:19 +0900

    update: abc237b

--- cpp/abc237/b/main.cpp
+++ cpp/abc237/b/main.cpp
@@ -5,6 +5,7 @@ int main() {
     int h, w;
     cin >> h >> w;
     int a[h][w];
+    int b[w][h];
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) {
             cin >> a[i][j];
@@ -12,7 +13,12 @@ int main() {
     }
     for (int i = 0; i < w; i++) {
         for (int j = 0; j < h; j++) {
-            cout << a[j][i] << (j != h-1 ? " " : "\n");
+            b[i][j] = a[j][i];
+        }
+    }
+    for (int i = 0; i < w; i++) {
+        for (int j = 0; j < h; j++) {
+            cout << b[i][j] << (j != h-1 ? " " : "\n");
         }
     }
 }
