a7d8fcf671cf8571ee9510becc758b72493ec92a
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-07 00:34:07 +0900

    update: abc229b

--- cpp/abc229/b/main.cpp
+++ cpp/abc229/b/main.cpp
@@ -5,8 +5,7 @@ int main() {
     long long a, b;
     cin >> a >> b;
     while (a > 0 && b > 0) {
-        int x = a % 10 + b % 10;
-        if (x > 9) {
+        if ((a%10) + (b%10) >= 10) {
             cout << "Hard" << endl;
             return 0;
         }
