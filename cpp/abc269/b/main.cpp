5d5fd00a46c2ae9fe8d55c61d3756ddb50e2cf3b
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-07 19:19:52 +0900

    update: abc269b

--- cpp/abc269/b/main.cpp
+++ cpp/abc269/b/main.cpp
@@ -2,16 +2,18 @@
 using namespace std;
 
 int main() {
-    int a = 11, b = 0, c = 11, d = 0;
-    for (int i = 1; i <= 10; i++) {
-        string s;
-        cin >> s;
-        for (int j = 1; j <= 10; j++) {
-            if (s[j-1] == '#') {
-                a = min(a, i);
-                b = max(b, i);
-                c = min(c, j);
-                d = max(d, j);
+    vector<string> s(10);
+    for (int i = 0; i < 10; i++) cin >> s[i];
+
+    int a = 1e9, b = -1e9;
+    int c = 1e9, d = -1e9;
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (s[i][j] == '#') {
+                a = min(a, i+1);
+                b = max(b, i+1);
+                c = min(c, j+1);
+                d = max(d, j+1);
             }
         }
     }
