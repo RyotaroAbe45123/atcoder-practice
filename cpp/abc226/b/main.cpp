f53ea454078fdb6cd6d72e27a3203b021a0438fc
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-07 17:22:46 +0900

    update: abc266b

--- cpp/abc226/b/main.cpp
+++ cpp/abc226/b/main.cpp
@@ -4,32 +4,14 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    vector<int> l(n);
-    int a[n][n];
-    vector<int> c(n, 0);
-
+    set<vector<int>> st;
     for (int i = 0; i < n; i++) {
-        cin >> l[i];
-        for (int j = 0; j < n; j++) cin >> a[i][j];
+        int l;
+        cin >> l;
+        vector<int> v(l);
+        for (int j = 0; j < l; j++) cin >> v[j];
+        st.insert(v);
     }
-    for (int i = 0; i < n; i++) {
-        bool found = false;
-        for (int j = 0; j < i; j++) {
-            if (l[i] == l[j]) {
-                bool same = true;
-                for (int k = 0; k < l[i]; k++) {
-                    if (a[i][k] != a[j][k]) {
-                        same = false;
-                        break;
-                    }
-                }
-                if (same) found = true;
-            }
-        }
-        if (!found) c[i]++;
-    }
-    int ans = 0;
-    for (int i = 0; i < n; i++) ans += c[i];
-    cout << ans << endl;
+    cout << st.size() << endl;
     return 0;
 }
