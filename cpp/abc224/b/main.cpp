19b06674d02545786e01a72736a6026c0d5e1a19
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-07 19:08:10 +0900

    update: abc244b

--- cpp/abc224/b/main.cpp
+++ cpp/abc224/b/main.cpp
@@ -8,18 +8,19 @@ int main() {
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) cin >> a[i][j];
     }
-    bool ok = true;
+
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) {
             for (int k = i+1; k < h; k++) {
                 for (int l = j+1; l < w; l++) {
                     if (a[i][j] + a[k][l] > a[k][j] + a[i][l]) {
-                        ok = false;
-                        break;
+                        cout << "No" << endl;
+                        return 0;
                     }
                 }
             }
         }
     }
-    cout << (ok ? "Yes" : "No") << endl;
+    cout << "Yes" << endl;
+    return 0;
 }
