7e4d60142318daa27d7e11257263b9e1cb97f7e6
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-08 22:26:37 +0900

    update: abc250b

--- cpp/abc250/b/main.cpp
+++ cpp/abc250/b/main.cpp
@@ -4,28 +4,20 @@ using namespace std;
 int main() {
     int n, a, b;
     cin >> n >> a >> b;
-    vector<string> tiles(n, string(n, '-'));
-    for (int i = 0; i < n; i++) {
-        if (i % 2 == 0) {
-            for (int j = 0; j < n; j++) {
-                if (j % 2 == 0) tiles[i][j] = '.';
-                else tiles[i][j] = '#';
+    vector<string> s(a*n, string(b*n, '-'));
+    for (int i = 0; i < a * n; i++) {
+        for (int j = 0; j < b * n; j++) {
+            if ((i/a)%2 == 0) {
+                if ((j/b)%2 == 0) s[i][j]= '.';
+                else s[i][j] = '#';
             }
-        }
-        else {
-            for (int j = 0; j < n; j++) {
-                if (j % 2 == 0) tiles[i][j] = '#';
-                else tiles[i][j] = '.';
+            else {
+                if ((j/b)%2 == 0) s[i][j]= '#';
+                else s[i][j] = '.';
             }
         }
     }
 
-    vector<string> s(a*n, string(b*n, '-'));
-    for (int i = 0; i < a * n; i++) {
-        for (int j = 0; j < b * n; j++) {
-            s[i][j] = tiles[i/a][j/b];
-        }
-    }
     for (int i = 0; i < a * n; i++) {
         cout << s[i] << endl;
     }
