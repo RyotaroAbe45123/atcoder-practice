d0451d1da55cb754b0fdd5f39e54b1dc13212862
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-10 08:50:55 +0900

    update: abc257b

--- cpp/abc257/b/main.cpp
+++ cpp/abc257/b/main.cpp
@@ -4,16 +4,15 @@ using namespace std;
 int main() {
     int n, k, q;
     cin >> n >> k >> q;
-    vector<int> a(k);
-    for (int i = 0; i < k; i++) cin >> a[i];
+    int a[202];
+    for (int i = 1; i <= k; i++) cin >> a[i];
+    a[k+1] = n + 1;
 
     for (int i = 0; i < q; i++) {
         int l;
         cin >> l;
-        if (a[l-1] == n) continue;
-        else if (l != k && a[l-1] == a[l] - 1) continue;
-        else a[l-1]++;
+        if (a[l] + 1 < a[l+1]) a[l]++;
     }
-    for (int i = 0; i < k; i++) cout << a[i] << (i == k-1 ? "\n" : " ");
+    for (int i = 1; i <= k; i++) cout << a[i] << (i == k ? "\n" : " ");
     return 0;
 }
