e9efbb72a8229eeda2b078d75ba723c28db7b958
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-11 21:47:39 +0900

    update: abc261b

--- cpp/abc261/b/main.cpp
+++ cpp/abc261/b/main.cpp
@@ -4,30 +4,22 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    vector<string> a(n);
+    vector<vector<char>> a(n, vector<char>(n));
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> a[i][j];
         }
     }
 
+    string ans = "correct";
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             if (i == j) continue;
-            if (a[i][j] == 'W' && a[j][i] != 'L') {
-                cout << "incorrect" << endl;
-                return 0;
-            }
-            if (a[i][j] == 'L' && a[j][i] != 'W') {
-                cout << "incorrect" << endl;
-                return 0;
-            }
-            if (a[i][j] == 'D' && a[j][i] != 'D') {
-                cout << "incorrect" << endl;
-                return 0;
-            }
+            if (a[i][j] == 'W' && a[j][i] != 'L') ans = "incorrect";
+            if (a[i][j] == 'L' && a[j][i] != 'W') ans = "incorrect";
+            if (a[i][j] == 'D' && a[j][i] != 'D') ans = "incorrect";
         }
     }
-    cout << "correct" << endl;
+    cout << ans << endl;
     return 0;
 }
