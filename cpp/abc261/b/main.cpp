4907be705df8a570a830face1c487d9781bbb80c
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-11 21:45:50 +0900

    update: abc260n

--- cpp/abc261/b/main.cpp
+++ cpp/abc261/b/main.cpp
@@ -14,26 +14,17 @@ int main() {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             if (i == j) continue;
-            if (a[i][j] == 'W') {
-                if (a[j][i] != 'L') {
-                    cout << "incorrect" << endl;
-                    return 0;
-                }
-                else continue;
+            if (a[i][j] == 'W' && a[j][i] != 'L') {
+                cout << "incorrect" << endl;
+                return 0;
             }
-            else if (a[i][j] == 'L') {
-                if (a[j][i] != 'W') {
-                    cout << "incorrect" << endl;
-                    return 0;
-                }
-                else continue;
+            if (a[i][j] == 'L' && a[j][i] != 'W') {
+                cout << "incorrect" << endl;
+                return 0;
             }
-            else if (a[i][j] == 'D') {
-                if (a[j][i] != 'D') {
-                    cout << "incorrect" << endl;
-                    return 0;
-                }
-                else continue;
+            if (a[i][j] == 'D' && a[j][i] != 'D') {
+                cout << "incorrect" << endl;
+                return 0;
             }
         }
     }
