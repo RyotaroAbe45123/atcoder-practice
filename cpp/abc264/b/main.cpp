b68a2eb31b016710a2484243d5d3cb011732c8a5
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-12 22:41:12 +0900

    add: abc264b

--- cpp/abc264/b/main.cpp
+++ cpp/abc264/b/main.cpp
@@ -4,21 +4,7 @@ using namespace std;
 int main() {
     int r, c;
     cin >> r >> c;
-    if (r % 2 == 0) {
-        if (r <= c || c <= 15-r) {
-            cout << "white" << endl;
-        }
-        else {
-            cout << (c % 2 == 0 ? "white" : "black") << endl;
-        }
-    }
-    else {
-        if (r <= c || c <= 15-r) {
-            cout << "black" << endl;
-        }
-        else {
-            cout << (c % 2 == 0 ? "black" : "white") << endl;
-        }
-    }
+    if (max(abs(r - 8), abs(c - 8)) % 2) cout << "black" << endl;
+    else cout << "white" << endl;
     return 0;
 }
