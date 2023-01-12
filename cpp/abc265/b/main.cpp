27e24cc77155b7a8db996c0c7b112d77757a3c32
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-12 23:00:50 +0900

    add: abc265b

---
+++ cpp/abc265/b/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    long long t;
+    cin >> n >> m >> t;
+    vector<long long> a(n-1);
+    vector<int> x(m);
+    vector<long long> y(m);
+    for (int i = 0; i < n - 1; i++) cin >> a[i];
+    for (int i = 0; i < m; i++) {
+        cin >> x[i] >> y[i];
+    }
+    int idx = 0;
+    string ans = "No";
+    while (t > 0) {
+        t -= a[idx];
+        if (t > 0) idx++;
+        else break;
+
+        for (int j = 0; j < m; j++) {
+            if (idx == x[j]-1) t += y[j];
+        }
+        if (idx == n - 1) ans = "Yes";
+    }
+    cout << ans << endl;
+    return 0;
+}
