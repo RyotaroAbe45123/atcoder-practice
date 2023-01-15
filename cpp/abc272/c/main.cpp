574a12e5b804682c71190cd4e729838e847b3c29
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-16 08:28:25 +0900

    update: abc272c

--- cpp/abc272/c/main.cpp
+++ cpp/abc272/c/main.cpp
@@ -7,15 +7,15 @@ int main() {
     vector<long long> a(n);
     for (int i = 0; i < n; i++) cin >> a[i];
 
-    long long ans = -1;
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
+    for (int i = n-1; i >= 0; i--) {
+        for (int j = i - 1; j >= 0; j--) {
             long long x = a[i] + a[j];
             if (x % 2 == 0) {
-                ans = max(ans, x);
+                cout << x << endl;
+                return 0;
             }
         }
     }
-    cout << ans << endl;
+    cout << "-1" << endl;
     return 0;
 }
