d2d944ceb221ece0e90e20cb4a68042ad0990d30
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-16 07:46:04 +0900

    update: abc285b

--- cpp/abc285/b/main.cpp
+++ cpp/abc285/b/main.cpp
@@ -5,31 +5,17 @@ int main() {
     int n;
     string s;
     cin >> n >> s;
-    vector<bool> x(n-1, false);
-    for (int i = 0; i < n-1; i++) {
-        bool f = true;
-        for (int j = 1; j < n; j++) {
-            if (s[i-1] == s[i+j-1]) f = false;
-        }
-        if (f) x[i] = true;
-
-    }
     for (int i = 1; i < n; i++) {
-        int ans = 0;
-        for (int l = n-i; l > 0; l--) {
-            bool f = true;
-            for (int k = 1; k <= l; k++) {
-                if (s[k-1] == s[k+i-1]) {
-                    f = false;
-                    break;
-                }
-            }
-            if (f) {
-                ans = l;
+        for (int j = 1; j <= n; j++) {
+            if (i + j > n) {
+                cout << j - 1 << endl;
                 break;
-            };
+            }
+            if (s[j-1] == s[j+i-1]) {
+                cout << j - 1 << endl;
+                break;
+            }
         }
-        cout << ans << endl;
     }
     return 0;
 }
