57d564303d2e6c5b4848a144de5a222d649a25d4
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-17 22:19:34 +0900

    add: abc271c

---
+++ cpp/abc271/c/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<bool> vol(n, false);
+    vector<long long> a(n);
+
+    int sold = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        if (a[i] >= vol.size()) sold++;
+        else if (vol[a[i]]) sold++;
+        else vol[a[i]] = true;
+    }
+
+    int L = 1;
+    int R = n + 1;
+    while (true) {
+        while (vol[L]) {
+            L++;
+        }
+        while (R != 0 && !vol[R]) {
+            R--;
+        }
+        if (sold >= 2) {
+            sold -= 2;
+            vol[L] = true;
+        }
+        else {
+            if (L >= R) break;
+            vol[R] = false;
+            sold++;
+        }
+    }
+
+    cout << L - 1 << endl;
+    return 0;
+}
