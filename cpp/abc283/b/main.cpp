90ca9c4843dbf7daa07ba99ae3332b0104d914fd
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-21 00:27:03 +0900

    update: abc283b

--- cpp/abc283/b/main.cpp
+++ cpp/abc283/b/main.cpp
@@ -2,21 +2,23 @@
 using namespace std;
 
 int main() {
-    int n, q;
-    cin >> n >> q;
-    vector<vector<int>> a(n);
-    for (int i = 0; i < n; ++i) {
-        int l;
-        cin >> l;
-        a[i] = vector<int>(l);
-        for (int j = 0; j < l; ++j) {
-            cin >> a[i][j];
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        int l, k;
+        cin >> l >> k;
+        if (l == 1) {
+            int x;
+            cin >> x;
+            a[k-1] = x;
         }
-    }
-    for (int k = 0; k < q; ++k) {
-        int s, t;
-        cin >> s >> t;
-        cout << a[s - 1][t - 1] << endl;
+        else if (l == 2) {
+            cout << a[k-1] << endl;
+        }
     }
     return 0;
 }
