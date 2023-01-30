6cfa72a8ad1ee7956498435894be6909d0f2439d
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-30 22:39:42 +0900

    update

--- cpp/abc286/a/main.cpp
+++ cpp/abc286/a/main.cpp
@@ -6,4 +6,19 @@ int main() {
     cin >> n >> p >> q >> r >> s;
     vector<int> a(n);
     for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) {
+        if (i < p-1) {
+            cout << a[i] << endl;
+        }
+        else if (p-1 < i < q-1) {
+            cout << a[r-i-p-1] << endl;
+        }
+        else if (r-1 < i < s-1) {
+            cout << a[i-r+p-1] << endl;
+        }
+        else {
+            cout << a[i] << endl;
+        }
+    }
+    
 }
