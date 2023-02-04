1c96af74e6ee9f9a4bcdfbf32ffbe2fc3f4ef8ff
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-02-04 20:46:35 +0900

    update: abc166c

--- cpp/abc166/c/main.cpp
+++ cpp/abc166/c/main.cpp
@@ -17,15 +17,12 @@ int main() {
         G[b].push_back(a);
     }
     int ans = 0;
-    for (int i = 0; i < m; i++) {
-        if (G[i].size() == 1) ans++;
-        else {
-            bool flag = true;
-            for (int j = 0; j < G[i].size(); j++) {
-                if (H[G[i][j]] > H[i]) flag = false;
-            }
-            if (!flag) ans++;
+    for (int i = 0; i < n; i++) {
+        bool flag = true;
+        for (int j = 0; j < (int)G[i].size(); j++) {
+            if (H[G[i][j]] >= H[i]) flag = false;
         }
+        if (flag) ans++;
     }
     cout << ans << endl;
     return 0;
