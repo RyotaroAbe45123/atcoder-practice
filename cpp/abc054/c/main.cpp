3d12b400ae201081d8b5fa667c25c18570513194
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-02-05 21:28:10 +0900

    update: abc054c

--- cpp/abc054/c/main.cpp
+++ cpp/abc054/c/main.cpp
@@ -1,31 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using Graph = vector<vector<int>>;
+const int nmax = 8;
+bool graph[nmax][nmax];
 
-vector<bool> seen;
-void dfs(const Graph &G, int v) {
-    seen[v] = true;
+int dfs(int v, int N, bool visited[nmax]) {
+    bool all_visited = true;
 
-    for (auto next_v : G[v]) {
-        if (seen[next_v]) continue;
-        dfs(G, next_v);
+    for (int i = 0; i < N; i++) {
+        if (visited[i] == false) {
+            all_visited = false;
+        }
     }
+
+    if (all_visited) {
+        return 1;
+    }
+
+    int ret = 0;
+
+    for (int i = 0; i < N; i++) {
+        if (graph[v][i] == false) continue;
+        if (visited[i]) continue;
+
+        visited[i] = true;
+        ret += dfs(i, N, visited);
+        visited[i] = false;
+    }
+    return ret;
 }
 
 int main() {
     int n, m;
     cin >> n >> m;
-    Graph G(n);
     for (int i = 0; i < m; i++) {
         int a, b;
         cin >> a >> b;
         a--, b--;
-        G[a].push_back(b);
-        G[b].push_back(a);
+        graph[a][b] = graph[b][a] = true;
+    }
+    bool visited[nmax];
+    for (int i = 0; i < n; i++) {
+        visited[i] = false;
     }
 
-    seen.assign(n, false);
-    dfs(G, 0);
+    visited[0] = true;
+    cout << dfs(0, n, visited) << endl;
     return 0;
 }
