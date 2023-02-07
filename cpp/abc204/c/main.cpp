639029c1fd336678781c1abd0b85423c3b9380f3
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-02-07 21:55:38 +0900

    update

--- cpp/abc204/c/main.cpp
+++ cpp/abc204/c/main.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int MAX_N = 2000;
+
+vector<vector<int>> G;
+bool tmp[MAX_N];
+void dfs(int v) {
+    if (tmp[v]) return;
+    tmp[v] = true;
+    for (auto vv : G[v]) dfs(vv);
+}
+
 int main() {
     int n, m;
     cin >> n >> m;
