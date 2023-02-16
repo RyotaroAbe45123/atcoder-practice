0f492e6fff41a968944caf7edf6a402c75607b9e
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-02-16 23:24:01 +0900

    update

--- cpp/abc289/c/main.cpp
+++ cpp/abc289/c/main.cpp
@@ -5,5 +5,11 @@ int main() {
     int n, m;
     cin >> n >> m;
     vector<vector<int>> a(m);
+    for (int i = 0; i < m; i++) {
+        int c;
+        cin >> c;
+        a[i].resize(c);
+        for (auto& x : a[i]) cin >> x;
+      }
     return 0;
 }
