c738f107fe39b08aafdaa3d22d5614b0b9496f07
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-02-17 07:14:39 +0900

    update

--- cpp/abc289/c/main.cpp
+++ cpp/abc289/c/main.cpp
@@ -10,6 +10,17 @@ int main() {
         cin >> c;
         a[i].resize(c);
         for (auto& x : a[i]) cin >> x;
-      }
-    return 0;
+    }
+    int ans = 0;
+    for (int b = 0; b < (1 << m); b++) {
+        set<int> s;
+        for (int i = 0; i < m; i++) {
+        if ((b >> i) & 1) {
+            for (auto& x : a[i]) s.insert(x);
+        }
+        }
+        ans += (int)s.size() == n;
+    }
+    cout << ans << "\n";
+return 0;
 }
