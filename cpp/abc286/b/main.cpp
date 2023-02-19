d0dcc283cb4c5543fa4fd30fedd88a8f13af4377
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-02-19 23:07:54 +0900

    ¶update¶

--- cpp/abc286/b/main.cpp
+++ cpp/abc286/b/main.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
+    string s;
+    cin >> s;
+    for (int i = 0; i < (int)s.size(); i++) {
+        if (i != s.size() -1 && s[i] == 'n' && s[i+1] == 'a') {
+            cout << "nya";
+            i++;
+        } else {
+            cout << s[i];
+        }
+    }
+    return 0;
 }
